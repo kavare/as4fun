0d19cf45114830f9e483bdf5a674dc2264926336
kavare <aarontthsieh@gmail.com>
2015-02-28 14:22:38 +0800

    update to wp 4.1.1; adding .gitignore

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -1,44 +1,44 @@
 <?php
-/**
- * The base configurations of the WordPress.
+/** 
+ * WordPress 基本設定檔。
  *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, and ABSPATH. You can find more information by visiting
- * {@link http://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
- * Codex page. You can get the MySQL settings from your web host.
+ * 本檔案包含以下設定選項： MySQL 設定、資料表前綴、
+ * 私密金鑰、WordPress 語言設定以及 ABSPATH。如需更多資訊，請
+ * 前往 {@link http://codex.wordpress.org/Editing_wp-config.php 編輯
+ * wp-config.php} Codex 頁面。或者向您的空間提供商諮詢關於 MySQL 設定資訊。
  *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
+ * 這個檔案用於安裝程式自動生成 wp-config.php 設定檔。
+ * 您不需要將它用於您的網站，可以手動複製這個檔案，
+ * 並重新命名為 "wp-config.php"，然後輸入相關訊息。
  *
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
+// ** MySQL 設定 - 您可以從主機服務提供商獲取相關資訊。 ** //
+/** WordPress 的資料庫名稱，請更改 "database_name_here" */
 define('DB_NAME', 'database_name_here');
 
-/** MySQL database username */
+/** MySQL 資料庫使用者名稱，請更改 "username_here" */
 define('DB_USER', 'username_here');
 
-/** MySQL database password */
+/** MySQL 資料庫密碼，請更改 "password_here" */
 define('DB_PASSWORD', 'password_here');
 
-/** MySQL hostname */
+/** MySQL 主機位址 */
 define('DB_HOST', 'localhost');
 
-/** Database Charset to use in creating database tables. */
+/** 建立資料表時預設的文字編碼 */
 define('DB_CHARSET', 'utf8');
 
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+/** 資料庫對照型態。如果不確定請勿更改。 */
+define('DB_COLLATE', 'utf8_unicode_ci');
 
 /**#@+
- * Authentication Unique Keys and Salts.
+ * 認證唯一金鑰設定。
  *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ * 將這些更改為不同的唯一字串或符號。
+ * 您可以使用 {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org 私密金鑰服務} 來自動產生。
+ * 您可於任何時候修改這些字串讓 Cookies 失效。這將會強制所有使用者必須重新登入。
  *
  * @since 2.6.0
  */
@@ -54,27 +54,61 @@ define('NONCE_SALT',       'put your unique phrase here');
 /**#@-*/
 
 /**
- * WordPress Database Table prefix.
+ * WordPress 資料表前綴。
  *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
+ * 若您為每個 WordPress 設定不同的資料表前綴，則可在同個資料庫內安裝多個 WordPress。
+ * 前綴只能使用半型數字、字母和底線！
  */
 $table_prefix  = 'wp_';
 
 /**
- * For developers: WordPress debugging mode.
+ * WordPress 自動儲存間隔
  *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
+ * 當您編輯文章時 WordPress 使用 Ajax 技術自動地定時幫您儲存文章草稿。
+ * 您可更改數值以延長或減少自動儲存的時間間隔。
+ * 預設儲存間隔為 60 秒。
+ */
+//define('AUTOSAVE_INTERVAL', 60 );  // 單位：秒
+
+/**
+ * WordPress 文章版本設定
+ *
+ * WordPress 預設會幫您儲存舊版的文章與分頁，以便您之後可以回復到先前的版本。
+ * 這功能可關閉，或是指定最大版本數量。
+ * 預設為開啟，若要關閉請將它設為 false。
+ * 若您想指定指定最大版本數量，請設個整數。
+ */
+//define('WP_POST_REVISIONS', true );
+
+/**
+ * 快取
+ *
+ * 若 WP_CACHE 值為 true，當它執行 wp-settings.php 時會把 wp-content/advanced-cache.php 一起執行。
+ * 許多快取外掛會要求您將這個值設為 true。
+ */
+//define('WP_CACHE', false);
+
+/**
+ * 啟用多網誌站台與網誌網路功能
+ *
+ * 若 WP_ALLOW_MULTISITE 值為 true 可啟用多網誌站台功能。
+ */
+//define('WP_ALLOW_MULTISITE', false);
+
+/**
+ * 開發人員用： WordPress 偵錯模式。
+ *
+ * 將此設定為 true 將可開啟開發時的通知顯示。
+ * 強烈建議外掛與佈景主題開發人員使用 WP_DEBUG
+ * 於他們的開發環境中。
  */
 define('WP_DEBUG', false);
 
-/* That's all, stop editing! Happy blogging. */
+/* 設定完成，請儲存檔案。然後開始 Blogging 吧！ */
 
-/** Absolute path to the WordPress directory. */
+/** WordPress 目錄的絕對路徑。 */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
-/** Sets up WordPress vars and included files. */
+/** 設定 WordPress 變數和包含的檔案。 */
 require_once(ABSPATH . 'wp-settings.php');
