21599bd1b95fa095df5163ae2119a454e37b62c4
kavare <aarontthsieh@gmail.com>
2015-03-26 00:34:44 +0800

    [trivial] reorganize information structure and setup related templates

--- wp-content/themes/afterschool/search.php
+++ wp-content/themes/afterschool/search.php
@@ -9,11 +9,11 @@
 	<?php if ( have_posts() ) : ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php get_template_part( 'parts/content', get_post_format() ); ?>
 		<?php endwhile; ?>
 
 		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
+			<?php get_template_part( 'parts/content', 'none' ); ?>
 
 	<?php endif;?>
 
