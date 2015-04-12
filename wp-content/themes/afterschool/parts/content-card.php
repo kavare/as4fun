fcd1f9127760c3aa52946a548c69191c13c2ef0b
kavare <aarontthsieh@gmail.com>
2015-04-12 23:59:19 +0800

    [connection] complete card layouts

--- wp-content/themes/afterschool/parts/content-card.php
+++ wp-content/themes/afterschool/parts/content-card.php
@@ -7,16 +7,23 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <header>
-    <h2><a href="<?php the_permalink(); ?>">CARDO<?php the_title(); ?></a></h2>
-    <?php FoundationPress_entry_meta(); ?>
-  </header>
-  <div class="entry-content">
-    <?php the_content(__('Continue reading...', 'FoundationPress')); ?>
+<?php $as_post_class = 'card-item small-12 large-6 columns' ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class($as_post_class); ?>>
+  <a href="<?php the_permalink(); ?>">
+    <header class="card-header">
+      <div class="card-image-container">
+        <?php the_post_thumbnail('', array('class' => 'card-image') ); ?>
+      </div>
+      <h2 class="card-title"><?php the_title(); ?></h2>
+    </header>
+  </a>
+
+  <?php FoundationPress_entry_meta(); ?>
+  <div class="card-content">
+    <?php the_content(__('繼續閱讀', 'FoundationPress')); ?>
   </div>
-  <footer>
-    <?php $tag = get_the_tags(); if (!$tag) { } else { ?><p><?php the_tags(); ?></p><?php } ?>
+  <footer class="card-footer">
+    <a href="<?php the_permalink(); ?>" class="button primary">繼續閱讀</a>
   </footer>
   <hr />
 </article>
