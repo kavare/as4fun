68d69a0ae319dfd30dac2d342ae3a6a0f87e706d
kavare <aarontthsieh@gmail.com>
2015-04-14 00:34:23 +0800

    [connection] complete connection card list layout

--- wp-content/themes/afterschool/library/entry-meta.php
+++ wp-content/themes/afterschool/library/entry-meta.php
@@ -1,8 +1,10 @@
 <?php
 if(!function_exists('FoundationPress_entry_meta')) :
     function FoundationPress_entry_meta() {
-        echo '<time class="updated" datetime="'. get_the_time('c') .'">'. sprintf(__('Posted on %s at %s.', 'FoundationPress'), get_the_date(), get_the_time()) .'</time>';
-        echo '<p class="byline author">'. __('Written by', 'FoundationPress') .' <a href="'. get_author_posts_url(get_the_author_meta('ID')) .'" rel="author" class="fn">'. get_the_author() .'</a></p>';
+        echo '<div class="meta-container">';
+        echo '<span class="byline author">'. __('作者：', 'FoundationPress') .' <a href="'. get_author_posts_url(get_the_author_meta('ID')) .'" rel="author" class="fn">'. get_the_author() .'</a></span>';
+        echo '<time class="updated" datetime="'. get_the_time('c') .'">'. sprintf(__('%s', 'FoundationPress'), get_the_date('Y/m/d')) .'</time>';
+        echo '</div>';
     }
 endif;
 ?>
