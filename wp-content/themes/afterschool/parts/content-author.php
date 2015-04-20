01d0ed35655e6c2de1f34e68723c7d548ce2ddac
kavare <aarontthsieh@gmail.com>
2015-04-20 23:56:39 +0800

    [connection] setup author card style and disqus container margin

--- wp-content/themes/afterschool/parts/content-author.php
+++ wp-content/themes/afterschool/parts/content-author.php
@@ -8,15 +8,14 @@
 ?>
 
 <div class="author-card">
-  <span class="byline author">
     <?php echo get_avatar( get_the_author_meta( 'ID' )); ?>
-    <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn">
+    <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="author-link">
       <?php echo get_the_author(); ?>
     </a>
-    <?php echo get_the_author_meta('description'); ?>
-  </span>
-
-  <time class="updated" datetime="<?php echo get_the_time('c'); ?>">
-    <?php // echo sprintf(__('%s', 'FoundationPress'), get_the_date('Y/m/d')); ?>
-  </time>
+    <p class="author-intro">
+      <?php echo get_the_author_meta('description'); ?>
+    </p>
 </div>
+<time class="updated" datetime="<?php echo get_the_time('c'); ?>">
+  <?php // echo sprintf(__('%s', 'FoundationPress'), get_the_date('Y/m/d')); ?>
+</time>
