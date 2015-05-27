caa396c1387487bc050ba463afaabed3cd1fb1d0
kavare <aarontthsieh@gmail.com>
2015-05-27 17:59:17 +0800

    [links] setup link-brick structure

--- wp-content/themes/afterschool/library/shortcodes.php
+++ wp-content/themes/afterschool/library/shortcodes.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * [as_shortcode_home_section generate front-page sections]
+ * @param  [array]  $atts    [attributes from shortcodes]
+ * @param  [string] $content [content inside shortcode tags]
+ * @return [string]          [html tags that will be printed on the page]
+ */
 function as_shortcode_home_section($atts, $content = null) {
   $signature = array(
     'id' => 2,
@@ -28,34 +34,55 @@ function as_shortcode_home_section($atts, $content = null) {
   return $section;
 }
 
-function as_shortcode_community_links($atts, $content = null) {
+/**
+ * [as_shortcode_community_link generate links cards for community-list page]
+ * @param  [array]  $atts    [attributes from shortcodes]
+ * @param  [string] $content [content inside shortcode tags]
+ * @return [string]          [html tags that will be printed on the page]
+ */
+function as_shortcode_community_link($atts, $content = null) {
   $signature = array(
+    'title' => '',
     'url' => '',
-    'img' => '',
-    'title' => ''
   );
   extract(shortcode_atts($signature, $atts));
 
   $link =
   '<a href="' . $url . '">' .
-    '<img src="' . $img . '" alt="' . $title . '">' .
+    do_shortcode($content) .
     '<h3 class="section-title">' . $title . '</h3>' .
-    '<div class="section-content">' . $content . '</div>' .
   '</a>';
-
-  var_dump($url);
-  var_dump($img);
-  die;
 
   return $link;
 }
 
+function as_shortcode_link_image($atts, $content = null) {
+  $signature = array(
+    'src' => '',
+  );
+  extract(shortcode_atts($signature, $atts));
+
+  $img = '<img src="' . $src . '" alt="' . $src . '">';
+
+  return $img;
+}
+
+
+/**
+ * [Afterschool shortcodes registration list]
+ */
 function as_register_shortcodes(){
    add_shortcode('home-section', 'as_shortcode_home_section');
-   add_shortcode('community-link', 'as_shortcode_community_links');
+   add_shortcode('community-link', 'as_shortcode_community_link');
+   add_shortcode('link-image', 'as_shortcode_link_image');
 }
 
 add_action('init', 'as_register_shortcodes');
+
+/**
+ * [Afterschool shortcodes supporting area]
+ * @support [the_content, the_excerpt, widget, comments]
+ */
 add_filter('the_excerpt', 'do_shortcode');
 add_filter('widget_text', 'do_shortcode');
 add_filter('comment_text', 'do_shortcode');
--- wp-content/themes/afterschool/templates/community-links.php
+++ wp-content/themes/afterschool/templates/community-links.php
@@ -3,22 +3,41 @@
 Template Name: Community Links
 */
 get_header(); ?>
-<div class="row">
-  <div class="small-12 columns" role="main">
 
-  <?php /* Start loop */ ?>
-  <?php while (have_posts()) : the_post(); ?>
-    <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-      <header>
-        <h1 class="entry-title"><?php the_title(); ?></h1>
-      </header>
-      <div class="entry-content">
-        <?php the_content(); ?>
-      </div>
-    </article>
-  <?php endwhile; // End the loop ?>
+<?php while (have_posts()) : the_post(); ?>
+<?php get_template_part( 'parts/content', 'jumbotron' ); ?>
+<div class="row bricks-container">
 
-  </div>
+<?php // the_content(); ?>
+<article class="brick-item small-12 medium-6 large-4 columns end">
+  <a href="http://as4fun.dev/wp-content/themes/afterschool/assets/img/ballon.png">
+    <div class="brick-image-container">
+      <img class="brick-image" src="http://as4fun.dev/wp-content/themes/afterschool/assets/img/ballon.png" alt="">
+    </div>
+    <h2 class="brick-title">友善連結一</h2>
+    <p class="brick-content">友善連結一友善連結一友善連結一友善連結一友善連結一</p>
+  </a>
+</article>
+<article class="brick-item small-12 medium-6 large-4 columns end">
+  <a href="http://as4fun.dev/wp-content/themes/afterschool/assets/img/ballon.png">
+    <div class="brick-image-container">
+      <img class="brick-image" src="http://as4fun.dev/wp-content/themes/afterschool/assets/img/ballon.png" alt="">
+    </div>
+    <h2 class="brick-title">友善連結二</h2>
+    <p class="brick-content">友善連結二友善連結二友善連結二友善連結二友善連結二</p>
+  </a>
+</article>
+<article class="brick-item small-12 medium-6 large-4 columns end">
+  <a href="http://as4fun.dev/wp-content/themes/afterschool/assets/img/ballon.png">
+    <div class="brick-image-container">
+      <img class="brick-image" src="http://as4fun.dev/wp-content/themes/afterschool/assets/img/ballon.png" alt="">
+    </div>
+    <h2 class="brick-title">友善連結三</h2>
+    <p class="brick-content">友善連結三友善連結三友善連結三友善連結三友善連結三</p>
+  </a>
+</article>
+
 </div>
+<?php endwhile; // End the loop ?>
 
 <?php get_footer(); ?>
