805c1f19232bf77354550eb6f0062a215865aaef
kavare <aarontthsieh@gmail.com>
2015-05-27 23:58:02 +0800

    [link] complete link brick styling and animation

--- wp-content/themes/afterschool/templates/community-links.php
+++ wp-content/themes/afterschool/templates/community-links.php
@@ -9,31 +9,37 @@ get_header(); ?>
 <div class="row bricks-container">
 
 <?php // the_content(); ?>
-<article class="brick-item small-12 medium-6 large-4 columns end">
-  <a href="http://as4fun.dev/wp-content/themes/afterschool/assets/img/ballon.png">
+<article class="small-12 large-6 columns end">
+  <a class="brick-item clearfix" href="http://as4fun.dev/wp-content/themes/afterschool/assets/img/ballon.png">
     <div class="brick-image-container">
       <img class="brick-image" src="http://as4fun.dev/wp-content/themes/afterschool/assets/img/ballon.png" alt="">
     </div>
-    <h2 class="brick-title">友善連結一</h2>
-    <p class="brick-content">友善連結一友善連結一友善連結一友善連結一友善連結一</p>
+    <div class="brick-content">
+      <h2 class="brick-title">友善連結一</h2>
+      <p class="brick-text">友善連結一友善連結一友善連結一友善連結一友善連結一</p>
+    </div>
   </a>
 </article>
-<article class="brick-item small-12 medium-6 large-4 columns end">
-  <a href="http://as4fun.dev/wp-content/themes/afterschool/assets/img/ballon.png">
+<article class="small-12 large-6 columns end">
+  <a class="brick-item clearfix" href="http://as4fun.dev/wp-content/themes/afterschool/assets/img/ballon.png">
     <div class="brick-image-container">
       <img class="brick-image" src="http://as4fun.dev/wp-content/themes/afterschool/assets/img/ballon.png" alt="">
     </div>
-    <h2 class="brick-title">友善連結二</h2>
-    <p class="brick-content">友善連結二友善連結二友善連結二友善連結二友善連結二</p>
+    <div class="brick-content">
+      <h2 class="brick-title">友善連結二</h2>
+      <p class="brick-text">友善連結二友善連結二友善連結二友善連結二友善連結二</p>
+    </div>
   </a>
 </article>
-<article class="brick-item small-12 medium-6 large-4 columns end">
-  <a href="http://as4fun.dev/wp-content/themes/afterschool/assets/img/ballon.png">
+<article class="small-12 large-6 columns end">
+  <a class="brick-item clearfix" href="http://as4fun.dev/wp-content/themes/afterschool/assets/img/ballon.png">
     <div class="brick-image-container">
       <img class="brick-image" src="http://as4fun.dev/wp-content/themes/afterschool/assets/img/ballon.png" alt="">
     </div>
-    <h2 class="brick-title">友善連結三</h2>
-    <p class="brick-content">友善連結三友善連結三友善連結三友善連結三友善連結三</p>
+    <div class="brick-content">
+      <h2 class="brick-title">友善連結三</h2>
+      <p class="brick-text">友善連結三友善連結三友善連結三友善連結三友善連結三</p>
+    </div>
   </a>
 </article>
 
