c58fb53dd798987c79d3ba6c208663833051470f
kavare <aarontthsieh@gmail.com>
2015-06-06 14:47:09 +0800

    [contact] revise ajax-loader behavior

--- wp-content/themes/afterschool/js/components/contact-form.js
+++ wp-content/themes/afterschool/js/components/contact-form.js
@@ -10,28 +10,40 @@
 
       timer: null,
 
-      initEventHandler: function() {
-        asWpcf7.selector.widget.on('invalid.wpcf7 spam.wpcf7 mailsent.wpcf7 mailfailed.wpcf7', function(e) {
+      init: function() {
+        this.bindEventHandler();
+        this.replaceAjaxLoader();
+      },
+
+      bindEventHandler: function() {
+        this.selector.widget.on('invalid.wpcf7 spam.wpcf7 mailsent.wpcf7 mailfailed.wpcf7', function(e) {
+          asWpcf7.selector.widget.removeClass('loading');
           clearTimeout(asWpcf7.timer);
           asWpcf7.hideWpcf7Message();
         });
 
-        asWpcf7.selector.widget.on('submit.wpcf7', function(e) {
+        this.selector.widget.on('submit.wpcf7', function(e) {
 
         });
 
-        asWpcf7.selector.submitBtn.on('click', function() {
-
+        this.selector.submitBtn.on('click', function() {
+          asWpcf7.selector.widget.addClass('loading');
+          asWpcf7.replaceAjaxLoader();
         });
       },
 
       hideWpcf7Message: function() {
-        asWpcf7.timer = setTimeout(function() {
+        this.timer = setTimeout(function() {
           $('.wpcf7-response-output').slideUp(400);
         }, 5000);
+      },
+
+      replaceAjaxLoader: function() {
+        var path = location.origin + '/wp-content/themes/afterschool/assets/img/loader/comment.gif';
+        $('.wpcf7 .ajax-loader').attr('src', path);
       }
     };
 
-    asWpcf7.initEventHandler();
+    asWpcf7.init();
   });
 })(jQuery);
