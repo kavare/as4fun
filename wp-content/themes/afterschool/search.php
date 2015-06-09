5d501a6d208c9e63ced4e25a1a387876abbad028
kavare <aarontthsieh@gmail.com>
2015-06-10 00:44:55 +0800

    [search] revise searchbar style

--- wp-content/themes/afterschool/search.php
+++ wp-content/themes/afterschool/search.php
@@ -5,16 +5,27 @@
 	<div class="small-12 large-10 large-push-1 columns" role="main">
 
 		<?php do_action('foundationPress_before_content'); ?>
+		<header class="search-header">
+			<h2 class="search-title">
+				<span class="search-keyword"><?php echo get_search_query(); ?></span>的搜尋結果
+			</h2>
 
-		<form role="search" method="get" id="searchform" class="searchform searchbar" action="<?php echo home_url('/'); ?>">
-			<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" class="search-input" placeholder="請輸入搜尋內容">
-			<input type="submit" id="searchsubmit" value="搜尋" class="button search-btn">
-		</form>
-		<h2 class="search-title">
-			<span class="search-keyword"><?php echo get_search_query(); ?></span>
-			的搜尋結果
-			<span class="search-meta">約有<span class="search-count"><?php echo $wp_query->found_posts; ?></span>個結果</span>
-		</h2>
+			<form role="search" method="get" id="searchform" class="searchform searchbar clearfix" action="<?php echo home_url('/'); ?>">
+				<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" class="search-input" placeholder="請輸入搜尋內容">
+				<input type="submit" id="searchsubmit" value="搜尋" class="button search-btn">
+			</form>
+			<span class="search-meta">
+				約有<span class="search-count"><?php echo $wp_query->found_posts; ?></span>個結果，
+				<?php
+					$page = get_query_var('paged');
+					$total_pages = $wp_query->max_num_pages;
+
+					echo '這是第' . $page . '頁';
+					echo '（共' . $total_pages . '頁）';
+				?>
+			</span>
+
+		</header>
 
 		<div class="row list-container">
 			<?php if ( have_posts() ) : ?>
