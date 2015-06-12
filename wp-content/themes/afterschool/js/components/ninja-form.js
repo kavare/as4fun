9e4a0a196969c605af58b6e41dd415ebd24c1f18
kavare <aarontthsieh@gmail.com>
2015-06-12 23:57:37 +0800

    [hotfix] revise responsive form in mobile view; remove footer line social icon in large screen; add ninja-form ajax submission callback

--- wp-content/themes/afterschool/js/components/ninja-form.js
+++ wp-content/themes/afterschool/js/components/ninja-form.js
@@ -5,6 +5,7 @@
     asNinjaForm = {
       init: function() {
         asNinjaForm.showOtherInput('.with-friends', '.friends-num-wrap');
+        asNinjaForm.registerSubmitCallback();
       },
 
       showOtherInput: function(checkbox, control) {
@@ -13,6 +14,19 @@
         $(checkbox).on('click', function() {
           isChecked = $(this).prop('checked');
           isChecked ? $(control).fadeIn(200) : $(control).fadeOut(200);
+        });
+      },
+
+      registerSubmitCallback: function() {
+        $(document).on('submitResponse', function(e, response) {
+            var errorMsg = response.errors['required-general'];
+            var successMsg = response.success['success msg-成功訊息'];
+            if (successMsg) {
+              alert(successMsg);
+              return;
+            }
+
+            alert(errorMsg);
         });
       }
     };
