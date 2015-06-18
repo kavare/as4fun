be531796d4e7cea3ed0498cc3def06331b8eead5
kavare <aarontthsieh@gmail.com>
2015-06-18 13:14:58 +0800

    [feature] complete 404, 401, 403 page

--- wp-content/themes/afterschool/parts/content-403.php
+++ wp-content/themes/afterschool/parts/content-403.php
@@ -1,18 +1,28 @@
 <?php get_header(); ?>
 
-<div class="row">
-  <div class="small-12 columns" role="main">
+<div class="row error-page-container">
+  <div class="small-12 columns">
+    <h1 class="error-pagetitle">401 你的權限不足</h1>
+  </div>
+  <div class="small-12 large-6 columns">
+    <img src="<?php as_img_src( 'logos/logo-lg.png' ); ?>" alt="放心窩" class="error-img">
+  </div>
 
-    <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-      <div class="row">
-        <div class="small-12 columns">
-          <h1>403 Forbidden</h1>
-          <p>大哥，這邊你不能進來喔！</p>
-        </div>
-      </div>
-    </article>
+  <div class="small-12 large-6 columns error-main" role="main">
+    <p class="error-content">大哥，這邊你不能進來喔 試試別的地方吧！</p>
+    <div class="error-section">
+      <p class="error-content">你可以試著再找找看...</p>
+      <form role="search" method="get" id="searchform" class="searchform searchbar clearfix" action="<?php echo home_url('/'); ?>">
+        <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" class="search-input" placeholder="請輸入搜尋內容">
+        <input type="submit" id="searchsubmit" value="搜尋" class="button search-btn">
+      </form>
+    </div>
 
+    <div class="error-section button-group">
+      <p class="error-content">你也可以...</p>
+      <a href="javascript:history.back()" class="button btn-back">回上一頁</a>
+      <a href="<?php echo home_url(); ?>" class="button btn-home">回到首頁</a>
+    </div>
   </div>
 </div>
 <?php get_footer(); ?>
-
