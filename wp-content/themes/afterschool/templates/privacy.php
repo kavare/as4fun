d914bf7f074933d5a66d0d81d7188054ffa3df43
kavare <aarontthsieh@gmail.com>
2015-06-19 20:09:47 +0800

    [revise] complete page specific headfold image settings

---
+++ wp-content/themes/afterschool/templates/privacy.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Privacy
+*/
+get_header(); ?>
+
+<?php while (have_posts()) : the_post(); ?>
+<?php get_template_part( 'parts/content', 'jumbotron' ); ?>
+<div class="row main-content-container">
+  <div class="small-12 large-10 large-push-1 columns" role="main">
+    <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+      <div class="entry-content">
+        <?php the_content(); ?>
+      </div>
+    </article>
+  </div>
+</div>
+<?php endwhile; // End the loop ?>
+
+
+<?php get_footer(); ?>
