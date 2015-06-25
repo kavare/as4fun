91506eab97bfc8caf1d3125bef795409d002192f
kavare <aarontthsieh@gmail.com>
2015-06-25 10:40:45 +0800

    [calendar] revise events-archive-day layouts

--- wp-content/themes/afterschool/tribe-events/day/single-event.php
+++ wp-content/themes/afterschool/tribe-events/day/single-event.php
@@ -20,49 +20,63 @@ $has_venue = ( $venue_details ) ? ' vcard' : '';
 $has_venue_address = ( ! empty( $venue_details['address'] ) ) ? ' location' : '';
 
 ?>
+<?php
+  if ( has_post_thumbnail() ):
+    echo tribe_event_featured_image( null, 'medium' );
+  else:
 
-<!-- Event Cost -->
-<?php if ( tribe_get_cost() ) : ?>
-  <div class="tribe-events-event-cost">
-    <span><?php echo tribe_get_cost( null, true ); ?></span>
-  </div>
+?>
+<div class="tribe-events-event-image">
+  <a href="<?php tribe_event_link(); ?>">
+    <img src="<?php as_img_src( 'placeholder-activity.png' ) ?>"
+       alt="放心窩協會" class="card-image">
+  </a>
+</div>
 <?php endif; ?>
 
-<!-- Event Title -->
-<?php do_action( 'tribe_events_before_the_event_title' ) ?>
-<h2 class="tribe-events-list-event-title summary">
-  <a class="url" href="<?php echo esc_url( tribe_get_event_link() ); ?>" title="<?php the_title() ?>" rel="bookmark">
-    <?php the_title() ?>
-  </a>
-</h2>
-<?php do_action( 'tribe_events_after_the_event_title' ) ?>
+<div class="tribe-events-container">
 
-<!-- Event Meta -->
-<?php do_action( 'tribe_events_before_the_meta' ) ?>
-<div class="tribe-events-event-meta <?php echo $has_venue . $has_venue_address; ?>">
-
-  <!-- Schedule & Recurrence Details -->
-  <div class="updated published time-details">
-    <?php echo tribe_events_event_schedule_details() ?>
-  </div>
-
-  <?php if ( $venue_details ) : ?>
-    <!-- Venue Display Info -->
-    <div class="tribe-events-venue-details">
-      <?php echo implode( ', ', $venue_details ); ?>
-    </div> <!-- .tribe-events-venue-details -->
+  <!-- Event Cost -->
+  <?php if ( tribe_get_cost() ) : ?>
+    <div class="tribe-events-event-cost">
+      <span><?php echo tribe_get_cost( null, true ); ?></span>
+    </div>
   <?php endif; ?>
 
-</div><!-- .tribe-events-event-meta -->
-<?php do_action( 'tribe_events_after_the_meta' ) ?>
+  <!-- Event Title -->
+  <?php do_action( 'tribe_events_before_the_event_title' ) ?>
+  <h2 class="tribe-events-list-event-title summary">
+    <a class="url" href="<?php echo esc_url( tribe_get_event_link() ); ?>" title="<?php the_title() ?>" rel="bookmark">
+      <?php the_title() ?>
+    </a>
+  </h2>
+  <?php do_action( 'tribe_events_after_the_event_title' ) ?>
 
-<!-- Event Image -->
-<?php echo tribe_event_featured_image( null, 'medium' ) ?>
+  <!-- Event Meta -->
+  <?php do_action( 'tribe_events_before_the_meta' ) ?>
+  <div class="tribe-events-event-meta <?php echo $has_venue . $has_venue_address; ?>">
 
-<!-- Event Content -->
-<?php do_action( 'tribe_events_before_the_content' ) ?>
-<div class="tribe-events-list-event-description tribe-events-content description entry-summary">
-  <?php echo tribe_events_get_the_excerpt() ?>
-  <a href="<?php echo esc_url( tribe_get_event_link() ); ?>" class="tribe-events-read-more" rel="bookmark"><?php _e( 'Find out more', 'tribe-events-calendar' ) ?> &raquo;</a>
-</div><!-- .tribe-events-list-event-description -->
-<?php do_action( 'tribe_events_after_the_content' ) ?>
+    <!-- Schedule & Recurrence Details -->
+    <div class="updated published time-details">
+      <?php echo tribe_events_event_schedule_details() ?>
+    </div>
+
+    <?php if ( $venue_details ) : ?>
+      <!-- Venue Display Info -->
+      <div class="tribe-events-venue-details">
+        <?php echo implode( ', ', $venue_details ); ?>
+      </div> <!-- .tribe-events-venue-details -->
+    <?php endif; ?>
+
+  </div><!-- .tribe-events-event-meta -->
+  <?php do_action( 'tribe_events_after_the_meta' ) ?>
+
+
+  <!-- Event Content -->
+  <?php do_action( 'tribe_events_before_the_content' ) ?>
+  <div class="tribe-events-list-event-description tribe-events-content description entry-summary">
+    <?php the_excerpt() ?>
+    <a href="<?php echo esc_url( tribe_get_event_link() ); ?>" class="tribe-events-read-more" rel="bookmark">瞭解詳情</a>
+  </div><!-- .tribe-events-list-event-description -->
+  <?php do_action( 'tribe_events_after_the_content' ) ?>
+</div>
