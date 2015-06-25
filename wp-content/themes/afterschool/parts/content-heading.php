64d0118fe48a7512a4b2beff6a5d307fba8cacec
kavare <aarontthsieh@gmail.com>
2015-06-25 13:15:05 +0800

    [calendar] fix blog, press and tribe_event page title settings

--- wp-content/themes/afterschool/parts/content-heading.php
+++ wp-content/themes/afterschool/parts/content-heading.php
@@ -2,11 +2,33 @@
   <!-- <h1 class="page-title"><?php as_show_single_title($pageTitle); ?></h1> -->
   <h1 class="page-title">
     <?php
-      if (is_archive() and !is_tax()):
-        post_type_archive_title();
+      /**
+       * [The Title for The Event Calendar]
+       */
+      if (tribe_is_event()):
+        if( tribe_is_month() && !is_tax() ): // The Main Calendar Page
+            echo '社區活動時間表';
+        elseif( tribe_is_month() && is_tax() ): // Calendar Category Pages
+            echo '社區活動時間表' . ' &raquo; ' . single_term_title('', false);
+        elseif( tribe_is_event() && !is_single() ): // The Main Events List
+            echo '社區活動';
+        elseif( tribe_is_event() && is_single() ): // Single Events
+            echo '社區活動 - ' . get_the_title();
+        endif;
+
+      /**
+       * [The Original Title]
+       */
       else:
-        single_cat_title();
+        if (is_category( 'blog' ) or is_category( 'press' )):
+          single_cat_title();
+        elseif (is_archive() and !is_tax()):
+          post_type_archive_title();
+        else:
+          single_cat_title();
+        endif;
       endif;
+
     ?>
   </h1>
   <!-- <h2 class="page-subtitle"></h2> -->
